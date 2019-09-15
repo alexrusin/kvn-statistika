7f9a793d5beb09235f91e304e04302fda95cb461
Alex Rusin <alexrusin_2000@yahoo.com>
2019-09-15 07:38:36 -0700

    added database backup snapshots

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->command('snapshot:create db_backup')->weekly();
     }
 
     /**
