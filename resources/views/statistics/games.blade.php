753e8876295d2122ae514dcaefb3d5faa4290f86
Alex Rusin <alexrusin_2000@yahoo.com>
2020-08-01 14:43:04 -0700

    some fixes

--- resources/views/statistics/games.blade.php
+++ resources/views/statistics/games.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mt-4 text-2xl font-semibold text-center text-gray-800">Результаты игр квн</h1>
+    <h1 class="mt-4 text-2xl font-semibold text-center text-gray-800">Результаты игр КВН</h1>
     <p class="mt-2 text-center">Лучшие игры КВН определяются оценкой Марфина и общей эффективностью.  Для того, чтобы узнать как выступили команды в каждой из игр, нажмите на соответствующую игру КВН.<p>
     @foreach ($games as $season => $leagues)
         @php
@@ -9,7 +9,7 @@
         ksort($sortedLeagues);
         @endphp
         @foreach ($sortedLeagues as $key => $data)
-        <h2 class="p-4 mt-4 font-bold text-center bg-white text-l"> {{ $key == 1 ? 'Высшая лига квн' : 'Премьер лига квн' }} {{$season}} г.</h2>
+        <h2 class="p-4 mt-4 font-bold text-center bg-white text-l"> {{ $key == 1 ? 'Высшая лига КВН' : 'Премьер лига КВН' }} {{$season}} г.</h2>
         <statistics-games :games="{{json_encode($data)}}"></statistics-games>
         @endforeach
     @endforeach
